4a8ab0d20b1e1917209604bd327d226e057dfbd3
Alan Ryan <alanryan619@gmail.com>
2025-01-21 03:04:38 -0300

    feat,fix: improved styles and fixed image loading problem and improved the CSS, HTML, JS structure.

--- web/scripts/script.js
+++ web/scripts/script.js
@@ -1,35 +1,35 @@
+"use strict";
+
 const cardWrap = document.getElementById('card-wrap');
 const cardListImages = [
-    "./assets/img/project-php.webp",
-    "./assets/img/project-php-2.webp",
-    "./assets/img/project-php-3.avif"
+    "./assets/images/01.webp",
+    "./assets/images/02.webp",
+    "./assets/images/03.webp"
 ];
-let lastImageIndex = -1; 
+let lastImageIndex = -1;
 
 cardWrap.querySelectorAll('.card-list').forEach(card => {
     const imageElement = card.querySelector('.card-listImage img');
     let randomIndex;
 
-    do{
-      randomIndex = Math.floor(Math.random() * cardListImages.length); 
-    } while (randomIndex === lastImageIndex); 
-    
+    do {
+        randomIndex = Math.floor(Math.random() * cardListImages.length);
+    } while (randomIndex === lastImageIndex);
+
     imageElement.src = cardListImages[randomIndex];
-    lastImageIndex = randomIndex; 
+    lastImageIndex = randomIndex;
 });
 
-"use strict";
-
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
     const scrollTopButton = document.getElementById('scrollTopButton');
     if (window.scrollY > 200) {
-        scrollTopButton.style.display = 'block'; 
+        scrollTopButton.style.display = 'block';
     } else {
-        scrollTopButton.style.display = 'none'; 
+        scrollTopButton.style.display = 'none';
     }
 });
 
-document.getElementById('scrollTopButton').addEventListener('click', function() {
+document.getElementById('scrollTopButton').addEventListener('click', function () {
     window.scrollTo({
         top: 0,
         behavior: 'smooth'
