033fc5f8823889da1204d29caa66b1c4f31b9297
Alan Ryan <alanryan619@gmail.com>
2025-02-07 21:07:50 -0300

    feat: Increased several improvements in responsiveness.

--- web/scripts/script.js
+++ web/scripts/script.js
@@ -124,8 +124,35 @@ document.getElementById('scrollTopButton').addEventListener('click', function ()
 // =========================================================================
 //  FUNCIONALIDADE: BLOQUEIO DO MENU DE CONTEXTO (CLIQUE COM O BOTÃO DIREITO)
 // =========================================================================
-document.addEventListener("contextmenu", function (e) {
-    e.preventDefault();
+document.addEventListener("contextmenu", function (event) {
+    event.preventDefault();
+
+    console.info(
+        "Context menu (botão direito do mouse) desabilitado para proteger o conteúdo."
+    );
+
+    const mensagem = document.createElement("div");
+    mensagem.textContent =
+        "A funcionalidade do botão direito do mouse foi desabilitada nesta página.";
+
+    mensagem.style.cssText = `
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: rgba(0, 0, 0, 0.89);
+    color: white;
+    padding: 10px 20px;
+    border-radius: 5px;
+    z-index: 9999;
+    font-size: 16px;
+    `;
+
+    document.body.appendChild(mensagem);
+
+    setTimeout(() => {
+        document.body.removeChild(mensagem);
+    }, 3000);
 });
 
 // =========================================================================
