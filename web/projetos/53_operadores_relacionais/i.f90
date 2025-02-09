56e00d49b07027c11f838d1cb1bc384acdeb2df0
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-02-09 13:23:43 -0300

    feat: created new program.

---
+++ web/projetos/53_operadores_relacionais/i.f90
@@ -0,0 +1,17 @@
+PROGRAM operadores_relacionais
+    IMPLICIT NONE
+
+    PRINT *, ""
+    PRINT *, "Operadores Relacionais: Relações entre Valores"
+    PRINT *, "+-----------+-----------------------+--------+"
+    PRINT *, "| Operador  | Significado           | Forma  |"
+    PRINT *, "+-----------+-----------------------+--------+"
+    PRINT *, "| ==        | Igual a               | .EQ.   |"
+    PRINT *, "| /=        | Diferente de          | .NE.   |"
+    PRINT *, "| >         | Maior que             | .GT.   |"
+    PRINT *, "| <         | Menor que             | .LT.   |"
+    PRINT *, "| >=        | Maior ou igual a      | .GE.   |"
+    PRINT *, "| <=        | Menor ou igual a      | .LE.   |"
+    PRINT *, "+-----------+-----------------------+--------+"
+
+END PROGRAM operadores_relacionais
